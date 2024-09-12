06aac388c21f94dc4a644981293c6147416b745a
Pasterz <gamedev.ppasterz@gmail.com>
2024-09-12 17:51:37 +0200

    Ability System Component and Attribute Set

--- enc_temp_folder/dc36358a78dc52344820e94c2ae2a662/AuraPlayerState.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Player/AuraPlayerState.h"
-
-AAuraPlayerState::AAuraPlayerState(){
-	NetUpdateFrequency = 100.f; //jak czesto serwer bedzie probowac updatowac klientów
-}
