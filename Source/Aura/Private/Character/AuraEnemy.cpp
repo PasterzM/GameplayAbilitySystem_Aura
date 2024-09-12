72973ca54081e606e3d83c4c5c2947407ea03d78
Pasterz <gamedev.ppasterz@gmail.com>
2024-09-12 21:03:47 +0200

    Constructing the AXC and SC

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,10 +2,19 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
 #include "../Aura.h"
 
 AAuraEnemy::AAuraEnemy(){
 GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+
+AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+AbilitySystemComponent->SetIsReplicated(true);
+AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+
+
+AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 }
 
 void AAuraEnemy::HighlightActor(){
--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -2,7 +2,22 @@
 
 
 #include "Player/AuraPlayerState.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
 
 AAuraPlayerState::AAuraPlayerState(){
 	NetUpdateFrequency = 100.f; //jak czesto serwer bedzie probowac updatowac klientów
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+
+	NetUpdateFrequency = 100.f;
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const{
+	return AbilitySystemComponent;
 }
