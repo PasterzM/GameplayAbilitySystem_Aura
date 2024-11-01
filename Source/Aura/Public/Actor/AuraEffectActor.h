9954f78e1810520500f1ce1deaf707252767733e
Pasterz <gamedev.ppasterz@gmail.com>
2024-11-01 09:46:14 +0100

    Duration gameplay effects

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -23,5 +23,6 @@ class AURA_API AAuraEffectActor: public AActor{
 	UPROPERTY(EditAnywhere, BlueprintReadOnly,Category="Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
-
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,Category="Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
 };
