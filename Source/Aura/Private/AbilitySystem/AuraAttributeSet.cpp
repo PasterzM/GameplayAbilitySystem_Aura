8ae1a0bd31ca89fdbc5a2fba11c83a6403abef21
Pasterz <gamedev.ppasterz@gmail.com>
2024-11-07 23:57:14 +0100

    PostGameplayEffectExecute

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include <GameplayEffectExtension.h>
+#include <GameFramework/Character.h>
+#include <AbilitySystemBlueprintLibrary.h>
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "Net/UnrealNetwork.h"
 
@@ -8,7 +11,7 @@ UAuraAttributeSet::UAuraAttributeSet(){
 	UE_LOG(LogTemp, Warning, TEXT("UAuraAttributeSet::UAuraAttributeSet"));
 
 	//metoda stworzona przez makro ATTRIBUTE_ACCESSORS
-	InitHealth(50.f);	
+	InitHealth(50.f);
 	InitMaxHealth(200.f);
 	InitMana(50.f);
 	InitMaxMana(200.f);
@@ -22,6 +25,57 @@ void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue){
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if(Attribute == GetHealthAttribute()){
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+	}
+	//if(Attribute == GetMaxHealthAttribute()){
+	//	UE_LOG(LogTemp, Warning, TEXT("MaxHealth %f"), NewValue);
+	//}
+	if(Attribute == GetManaAttribute()){
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+	}
+	//if(Attribute == GetMaxManaAttribute()){
+	//	UE_LOG(LogTemp, Warning, TEXT("MaxMana %f"), NewValue);
+	//}
+}
+
+void UAuraAttributeSet::SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& props) const{
+	//Source = causer of the effect, Target = target of the effect (owner of this AS)
+	props.EffectContextHandle = Data.EffectSpec.GetContext();
+	props.SourceASC = props.EffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
+
+	if(IsValid(props.SourceASC) && props.SourceASC->AbilityActorInfo.IsValid() && props.SourceASC->AbilityActorInfo->AvatarActor.IsValid()){
+		props.SourceAvatarActor = props.SourceASC->AbilityActorInfo->AvatarActor.Get();
+		props.SourceController = props.SourceASC->AbilityActorInfo->PlayerController.Get();
+		if(props.SourceController == nullptr && props.SourceAvatarActor != nullptr){
+			if(const APawn* Pawn = Cast<APawn>(props.SourceAvatarActor)){
+				props.SourceController = Pawn->GetController();
+			}
+		}
+		if(props.SourceController){
+			ACharacter* SourceCharacter = Cast<ACharacter>(props.SourceController->GetPawn());
+		}
+	}
+
+	if(Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid()){
+		props.TargetAvatarActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
+		props.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+		props.TargetCharacter = Cast<ACharacter>(props.TargetAvatarActor);
+		props.TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(props.TargetAvatarActor);
+	}
+}
+
+void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data){
+	Super::PostGameplayEffectExecute(Data);
+	FEffectProperties props;
+	SetEffectProperties(Data, props);
+
+
 }
 
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)const{
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -14,12 +14,43 @@ GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
 GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
 GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
+USTRUCT()
+struct FEffectProperties{
+	GENERATED_BODY()
+
+	FEffectProperties(){}
+	FGameplayEffectContextHandle EffectContextHandle;
+
+	UPROPERTY()
+	UAbilitySystemComponent* SourceASC = nullptr;
+	UPROPERTY()
+	AActor* SourceAvatarActor = nullptr;
+	UPROPERTY()
+	AController* SourceController = nullptr;
+	UPROPERTY()
+	ACharacter* SourceCharacter = nullptr;
+
+	UPROPERTY()
+	UAbilitySystemComponent* TargetASC = nullptr;
+	UPROPERTY()
+	AActor* TargetAvatarActor = nullptr;
+	UPROPERTY()
+	AController* TargetController = nullptr;
+	UPROPERTY()
+	ACharacter* TargetCharacter = nullptr;
+};
+
 UCLASS()
 class AURA_API UAuraAttributeSet: public UAttributeSet{
 	GENERATED_BODY()
 	public:
 	UAuraAttributeSet();
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps)const override;
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+
 
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
 	FGameplayAttributeData Health;
@@ -46,4 +77,6 @@ class AURA_API UAuraAttributeSet: public UAttributeSet{
 	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
 	UFUNCTION()
 	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+	private:
+	void SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& props) const;
 };
